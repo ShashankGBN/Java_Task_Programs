f65405a56d73133924f481879f68a3ebfe877ac2
ShashankGBN <shashank.gowda@tibilsolutions.com>
2023-01-20 11:36:42 +0530

    one change made in print statement

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -6,7 +6,7 @@ public class LeapYear
 	public static void main(String[] args) 
 	{
 			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter a year to find whether it is leap year or not");
+			System.out.println("Enter a year :");
 			try
 			{
 			int year = sc.nextInt();
